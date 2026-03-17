90ff869f0127b66f835f5adc143336ce1ebc4690
KhazixW2 <3196497671@qq.com>
2026-03-17 12:41:10 +0800

    fix: 修复启动流程上可能遇到的问题，修复自动领取逻辑的问题

--- agent/action/zshg/daily_task.py
+++ agent/action/zshg/daily_task.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from math import log
 from maa.agent.agent_server import AgentServer
 from maa.context import Context
 from maa.custom_action import CustomAction
@@ -38,11 +37,11 @@ class DailyTaskProcessor(CustomAction):
             if not taskDetail.get("enabled"):
                 continue
 
-            logger.info(f"开始执行任务：{taskDetail['name']}")
+            logger.info(f"开始执行任务 {task_entry}")
 
             # 执行任务
             context.run_task(task_entry)
-            logger.info(f"任务执行完成：{taskDetail['name']}")
+            logger.info(f"任务执行完成 {task_entry}")
             context.run_task("UI_ReturnBigMap")
 
         return CustomAction.RunResult(success=True)
--- agent/agent_allfile.py
+++ agent/agent_allfile.py
@@ -2,5 +2,6 @@ import action.zshg.task_extractor
 
 import action.fight.fight_utils
 import action.fight.fight_processor
+import action.zshg.daily_task
 import action.zshg.child
 import action.zshg.marry
